9e7745edf05e69635e4d0c75a092268d1eb4348d
Monica Tamaru Jones <mntamaru@gmail.com>
2018-09-06 16:25:47 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,12 @@ class SearchesController < ApplicationController
 
   def search
   end
-
+  
+  def friends
+    foursquare = FoursquareService.new
+    @friends = foursquare.friends(session[:token])
+  end
+  
   def foursquare
     client_id = "CO3LIXJPH1LYAC5OOTLKLJE334NVDIYG24KUFOVEQ22WVYDP"
     client_secret = "0NNKMRWRYLCKLPSEE3G10I33WV0BTYXEN2JCJ41TVKKWB52Y"
